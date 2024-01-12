9bee24b98143107e5f9fa3feabf82a4f3d83a150
woo <suc2460@naver.com>
2024-01-12 18:50:13 +0900

    240112

--- BackServer/main.js
+++ BackServer/main.js
@@ -17,7 +17,10 @@ const db = require('./DB/DBconn');
 
 const port = 4000;
 
-app.use(express.static(path.join(__dirname,'reactapp/build/index.html')));
+const user = require('./router/userRouter');
+app.use('/api/userInfo',user);
+
+// app.use(express.static(path.join(__dirname,'reactapp/build/index.html')));
 app.use(bodyParser.json());
 app.set('port',port);
 
@@ -234,123 +237,6 @@ app.post('/api/lecture_Status',(req,res)=>{
         });
     });
 });
-//유저 정보 조회
-app.get('/api/userInfo/user',(req,res)=>{
-    console.log(req.query);
-    db.query(`
-    SELECT U.*, C1.CATEGORYNAME AS CNAME1, C2.CATEGORYNAME AS CNAME2,C3.CATEGORYNAME AS CNAME3
-    FROM USER U
-    JOIN CATEGORY C1 ON C1.CATEGORYID = U.CATEGORYID1
-    JOIN CATEGORY C2 ON C2.CATEGORYID = U.CATEGORYID2
-    JOIN CATEGORY C3 ON C3.CATEGORYID = U.CATEGORYID3
-    WHERE U.USERID ='${req.query.userId}';`,(err,result)=>{
-        if(err){
-            res.send('조회 실패');
-        }
-        console.log(result);
-        
-        res.send({UserInfo : result});
-    });
-});
-//유저 정보 수정
-app.post('/api/userInfo/user_update',(req,res)=>{
-    const userName = req.body.UserName;
-    const categoryId1 = req.body.CategoryID1;
-    const categoryId2 = req.body.CategoryID2;
-    const categoryId3 = req.body.CategoryID3;
-    const userId = req.body.UserID;
-    db.query(`
-    UPDATE USER
-    SET USERNAME = ?,CATEGORYID1 = ?,CATEGORYID2 =?,CATEGORYID3 =?
-    WHERE USERID = ?;`,[userName,categoryId1,categoryId2,categoryId3,userId],(err,result)=>{
-        if(err){
-            res.send('저장 실패');
-        }
-        console.log(result);
-        res.send('저장 성공');
-    });
-});
-//수강 내역
-app.get('/api/userInfo/study_lecture',(req,res)=>{
-    db.query(`
-    SELECT L.TITLE, E.PaymentStatus  FROM LECTURES L 
-    JOIN ENROLLMENTS E ON L.LECTUREID = E.LECTUREID 
-    WHERE E.USERID = '${req.query.UserID}' AND E.PaymentStatus IS TRUE ;`,(err,result)=>{
-        if(err){
-            res.send('조회 실패');
-        }
-        console.log(result);
-        res.send({
-            Code : 200,
-            Message : "응답 성공",
-            Study : result
-        });
-    });
-});
-//찜 목록
-app.get('/api/userInfo/nonstudy_lecture',(req,res)=>{
-    db.query(`
-    SELECT L.TITLE,L.LECTUREPAY ,L.BOOK ,L.LECTUREIMAGE ,E.PaymentStatus 
-    FROM LECTURES L 
-    JOIN ENROLLMENTS E ON L.LECTUREID = E.LECTUREID 
-    WHERE E.USERID = '${req.query.UserID}' AND E.PaymentStatus IS FALSE  ;`,(err,result)=>{
-        if(err){
-            res.send('조회 실패');
-        }
-        res.send({
-            Code : 200,
-            Message : "응답 성공",
-            nonStudy : result
-        });
-    });
-});
-
-//결제
-app.post('/api/payment',(req,res)=>{
-    const lectureId = req.body.LectureID;
-    const userId = req.body.UserID;
-    // const paymentDate = req.body.PaymentDate;
-    const pay = req.body.Pay;
-    const address = req.body.Address;
-    const name = req.body.Name;
-    const email = req.body.Email;
-    db.query(`
-    INSERT INTO PAYMENTS (LectureID,USERID,PAYMENTDATE,PAY,ADDRESS,NAME,EMAIL)
-    VALUE (?,?,NOW(),?,?,?,?);`,
-    [lectureId,userId,pay,address,name,email],(err,result)=>{
-        if(err){
-            res.send('실패');
-        }
-        db.query(`
-        UPDATE ENROLLMENTS SET PAYMENTSTATUS = TRUE
-        WHERE USERID = ? AND LECTUREID = ?;`,[userId,lectureId],(err,stupdate)=>{
-            if(err){
-                res.send('결제 유무확인 불가');
-            }
-            res.send('구매 성공~')
-        });
-    });
-});
-//결제 내역
-app.post('/api/userInfo/payment_list',(req,res)=>{
-    const userId = req.body.UserID;
-    db.query(`
-    SELECT P.PAY, P.PAYMENTDATE , L.TITLE,L.LECTUREPAY ,L.BOOK AS B_PAY,P.BOOK 
-    FROM PAYMENTS P
-    JOIN LECTURES L ON L.LECTUREID = P.LECTUREID
-    WHERE P.USERID = ?;`,[userId],(err,result)=>{
-        if(err){
-            res.send('결제 내역 조회 실패ㅜㅜ');
-        }
-        console.log(result);
-        res.send({
-            Code : 200,
-            Message : "응답 성공",
-            PaymentList : result
-        });
-    });
-});
-
 
 
 
